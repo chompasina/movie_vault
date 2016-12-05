be3125b5c12261b850422481d92f0f6d1bbae6ed
Tommasina <tommasina1@gmail.com>
2016-12-05 14:16:45 -0700

    Refines tests: - Adds model tests for user and movie - Adds sad path for invalid user login credentials - Comments out Api functionality until necessary

--- spec/features/user/user_can_sign_in_spec.rb
+++ spec/features/user/user_can_sign_in_spec.rb
@@ -12,4 +12,17 @@ RSpec.feature "User can sign in" do
     expect(page).to have_content("Hi, tommasina")
     expect(current_path).to eq(root_path)
   end
+  
+  scenario "User fills in incorrect credentials" do
+    user = create(:user)
+    
+    visit root_path
+    fill_in "Username", with: "tommasina"
+    fill_in "Password", with: "wrongpassword"
+    click_button "Login"
+    
+    expect(page).to_not have_content("Hi, tommasina")
+    expect(page).to have_content("Invalid credentials. Please try again.")
+    expect(current_path).to eq(login_path)
+  end
 end
